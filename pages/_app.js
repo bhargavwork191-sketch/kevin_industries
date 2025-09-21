256556c2584ceb2228d8be79e64b8caa92a0e0db
Bhargav Trivedi <bpt19196@gmail.com>
2025-09-21 19:51:43 +0530

    fixed: responsive design.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import { AuthProvider } from '../contexts/AuthContext'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
       <Component {...pageProps} />
     </AuthProvider>
   )
