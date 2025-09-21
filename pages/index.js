b2b0f1341f0e76118e38f8f2a20c06eeabdea9c5
Bhargav Trivedi <bpt19196@gmail.com>
2025-09-21 17:39:01 +0530

    Revert "fixed: image display issue."
    This reverts commit 5d55d89fdd9a882b78bb02b4b95f5dff2d369320.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 
 import Layout from '../components/Layout'
 import Carousel from '../components/Carousel'
-import ExcellenceImages from '../components/ExcellenceImages.js'
+import ExcellenceImages from '../components/ExcellenceImages'
 
 export default function Home() {
   return (
